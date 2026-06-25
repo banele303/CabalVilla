5099fcb3b4e634f70acff679b2fa8abdae05b7ca
blessed <sibandablessed724@gmail.com>
2026-06-25 09:52:05 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,35 @@
-import { Metadata } from "next"
-import Hero from "@/components/hero"
-import Services from "@/components/services"
-import Projects from "@/components/projects"
-import Footer from "@/components/footer"
-import Branches from "@/components/branches"
-import Menu from "@/components/menu"
+// import { Metadata } from "next"
+// import Hero from "@/components/hero"
+// import Services from "@/components/services"
+// import Projects from "@/components/projects"
+// import Footer from "@/components/footer"
+// import Branches from "@/components/branches"
+// import Menu from "@/components/menu"
 
-export const metadata: Metadata = {
-  title: "CabalVilla | Food, Drinks & Carwash in Tshivhulani, Lumveleni & Maniini",
-  description: "CabalVilla: For Food & Drinks Lovers. Great food, vibrant nightlife, and a top‑notch carwash. Visit our Tshivhulani, Lumveleni and Maniini branches.",
-  keywords: "CabalVilla, Tshivhulani, Lumveleni, Maniini, food, drinks, carwash, kota, pap and meat, beer, ciders, wine, live music",
-  openGraph: {
-    title: "CabalVilla | Eat. Sip. Shine.",
-    description: "Experience great food, refreshing drinks, live music, and premium carwash at CabalVilla.",
-    type: "website",
-    images: [
-      { url: "/hero2.jpeg", width: 1200, height: 630, alt: "CabalVilla" },
-    ],
-  },
-}
+// export const metadata: Metadata = {
+//   title: "CabalVilla | Food, Drinks & Carwash in Tshivhulani, Lumveleni & Maniini",
+//   description: "CabalVilla: For Food & Drinks Lovers. Great food, vibrant nightlife, and a top‑notch carwash. Visit our Tshivhulani, Lumveleni and Maniini branches.",
+//   keywords: "CabalVilla, Tshivhulani, Lumveleni, Maniini, food, drinks, carwash, kota, pap and meat, beer, ciders, wine, live music",
+//   openGraph: {
+//     title: "CabalVilla | Eat. Sip. Shine.",
+//     description: "Experience great food, refreshing drinks, live music, and premium carwash at CabalVilla.",
+//     type: "website",
+//     images: [
+//       { url: "/hero2.jpeg", width: 1200, height: 630, alt: "CabalVilla" },
+//     ],
+//   },
+// }
 
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <Hero />
+      <p>hosting error</p>
+      {/* <Hero />
       <Services />
       <Menu />
       <Projects />
       <Branches />
-      <Footer />
+      <Footer /> */}
     </main>
   )
 }
